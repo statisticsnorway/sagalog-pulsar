9c6d18af572cd6ae97f3dd0fc8e3df33ea48bbef
Kim Christian Gaarder <kim.christian.gaarder@gmail.com>
2019-06-27 15:08:21 +0200

    Improve admin test by avoid hardcoded topics

--- src/test/java/no/ssb/sagalog/pulsar/PulsarAdminTest.java
+++ src/test/java/no/ssb/sagalog/pulsar/PulsarAdminTest.java
@@ -17,38 +17,23 @@ public class PulsarAdminTest {
     public void thatBlahBlah() throws PulsarAdminException {
         PulsarSagaLogInitializer initializer = new PulsarSagaLogInitializer();
         Map<String, String> configuration = new LinkedHashMap<>(initializer.configurationOptionsAndDefaults());
-        configuration.put("cluster.owner", "distributed-saga-testng");
-        configuration.put("cluster.name", "pulsar-saga-log-pool-test");
+        String tenant = "distributed-saga-testng";
+        configuration.put("cluster.owner", tenant);
+        String namespace = "pulsar-saga-log-pool-test";
+        configuration.put("cluster.name", namespace);
         configuration.put("cluster.instance-id", "pool-test-01");
 
         PulsarSagaLogPool pool = initializer.initialize(configuration);
         PulsarAdmin admin = pool.admin;
 
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds01:00");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds01:01");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds01:02");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds01:03");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds01:04");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds01:05");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds02:00");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds02:01");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds02:02");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds02:03");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds02:04");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds02:05");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds03:00");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds03:01");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds03:02");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds03:03");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds03:04");
-        printStats(admin, "persistent://ssb.test/lds-cluster-test/lds03:05");
+        List<String> topics = admin.topics().getList(tenant + "/" + namespace);
+        for (String topic : topics) {
+            printStats(admin, topic);
+        }
     }
 
     private void printStats(PulsarAdmin admin, String topic) throws PulsarAdminException {
         Topics topics = admin.topics();
-
-        //JsonObject jsonObject = topics.getInternalInfo(topic);
-        //System.out.printf("%s%n", jsonObject.toString());
 
         TopicStats stats = topics.getStats(topic);
         List<ConsumerStats> masterConsumers = stats.subscriptions.get("master").consumers;
